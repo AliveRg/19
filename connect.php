5048d525fb30bb35e4915763421ad66283fbd6aa
AliveRg <renatgorodov49280@gmail.com>
2023-12-27 22:02:12 +0300

    add Password in users

--- connect.php
+++ connect.php
@@ -38,6 +38,7 @@
          user_id INT AUTO_INCREMENT PRIMARY KEY,
          user_name VARCHAR(255) NOT NULL,
          user_sername VARCHAR(255) NOT NULL,
+         password VARCHAR(255) NOT NULL,
          user_mail VARCHAR(255) NOT NULL
      );";
 
